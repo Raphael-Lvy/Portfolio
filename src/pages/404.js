4c072f7a1abc748a2b27789028dbdef8bc6b2c78
Raphael-Lvy <75268976+Raphael-Lvy@users.noreply.github.com>
2021-04-10 14:03:43 +0200

    error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Erreur</h1>
+    <p>Cette page n'existe pas... </p>
   </Layout>
 )
 
